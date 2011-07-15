e83498aa4293c563dd69a4c154d16671e0fdb670
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-07-15 12:56:36 +1200

    Added script to update docs from new releases.

---
+++ update-from-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+usage() {
+    echo "USAGE: update-from-release.sh CYLC_DIR"
+    echo "Replace PDF and HTML documentation with the same from"
+    echo "the specified new release."
+}
+
+if [[ $# != 1 ]]; then
+    usage
+    exit 1
+fi
+
+RELDIR=$1
+
+if [[ ! -x $RELDIR/bin/cylc ]]; then
+    usage
+    exit 1
+fi
+
+rm -f cug-html*.html cug-html.css
+rm -f CylcUserGuide.pdf
+
+# There may be extra screenshots in the web site graphics, so we don't 
+# remove and update these automatically.
+
+cp $RELDIR/doc/cug-html* .
+cp $RELDIR/doc/CylcUserGuide.pdf .
+
